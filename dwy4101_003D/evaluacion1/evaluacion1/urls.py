bf33c4f5ab9b1d556116b7eee89691406970332f
sebastian7573 <se.aguilar@alumnos.duoc.cl>
2020-11-07 21:33:13 -0300

    falta login

--- dwy4101_003D/evaluacion1/evaluacion1/urls.py
+++ dwy4101_003D/evaluacion1/evaluacion1/urls.py
@@ -6,7 +6,7 @@ from django.contrib.auth import views as auth_views
 
 
 urlpatterns = [
-    path('', auth_views.LoginView.as_view(redirect_authenticated_user=True)),
+    path('/', auth_views.LoginView.as_view(redirect_authenticated_user=True)),
     path('accounts/', include('django.contrib.auth.urls')),
     path('admin/', admin.site.urls),
     path('', include('gestorDeJuegos.urls')),
