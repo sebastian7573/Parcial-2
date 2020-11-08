9da7c1fdcea7f91fe96eb1f41b4c0c7d9bef00c0
sebastian7573 <se.aguilar@alumnos.duoc.cl>
2020-11-08 20:55:37 -0300

    Login, logout django listo

--- dwy4101_003D/evaluacion1/gestorDeJuegos/views.py
+++ dwy4101_003D/evaluacion1/gestorDeJuegos/views.py
@@ -5,12 +5,46 @@ from django.contrib import messages
 from django.contrib.auth.hashers import make_password
 from django.contrib.auth.models import User
 from django.conf import settings
+#PAL LOGIN
+from django.contrib.auth import authenticate
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login as do_login
+from django.contrib.auth import logout as do_logout
+
 
 # Create your views here.
 def index(request):
-    return render(request,'index.html',{})
+    # Si estamos identificados devolvemos la portada
+   # if request.user.is_authenticated:
+       return render(request,'index.html',{})
+     # En otro caso redireccionamos al login
+   # return redirect('/inicio')
+
 def inicio(request):
-    return render(request,'inicio.html',{})
+     # Creamos el formulario de autenticación vacío
+    form = AuthenticationForm()
+    if request.method == "POST":
+        # Añadimos los datos recibidos al formulario
+        form = AuthenticationForm(data=request.POST)
+        # Si el formulario es válido...
+        if form.is_valid():
+            # Recuperamos las credenciales validadas
+            nombreuser = form.cleaned_data["username"]
+            password = form.cleaned_data["password"]
+
+            # Verificamos las credenciales del usuario
+            user = authenticate(username=nombreuser, password=password)
+
+            # Si existe un usuario con ese nombre y contraseña
+            if user is not None:
+                # Hacemos el login manualmente
+                do_login(request, user)
+                # Y le redireccionamos a la portada
+                return redirect('/index')
+
+    # Si llegamos al final renderizamos el formulario
+    return render(request,'inicio.html',{'form': form})
+
 def registro(request):
     if request.method == "POST":
         nombre = request.POST["nombreuser"]
@@ -19,4 +53,8 @@ def registro(request):
         User.objects.create(username=nombre, email=correo, password=make_password(clave))
         return redirect(settings.LOGIN_REDIRECT_URL1, request.path)
     return render(request,'registro.html',{})
-
+def logout(request):
+    # Finalizamos la sesión
+    do_logout(request)
+    # Redireccionamos a la portada
+    return redirect('/')
